2ccba1daf86c357e345bdc2bff7d341b67dcf43e
ppxasjsm <antonia.mey@ed.ac.uk>
2020-03-13 15:36:36 +0000

    pH4 data for L

---
+++ data/L/gaff2/pH4/II_parmchk2.sh
@@ -0,0 +1,1 @@
+$AMBERHOME/bin/parmchk2 -i ade_L_ala_prep.mol2 -o ade_L_ala.frcmod -f mol2 -s 2
---
+++ data/L/gaff2/pH4/I_antechamber.sh
@@ -0,0 +1,3 @@
+#babel -ixyz ade-D-ala.xyz -opd ade-D-ala.pdb
+
+$AMBERHOME/bin/antechamber -i ade_L_ala.pdb -fi pdb -o ade_L_ala_prep.mol2 -fo mol2 -c 2 -eq 2 -at gaff2
