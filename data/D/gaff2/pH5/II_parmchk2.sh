4a7b882f8dd560bbe590b65f7ce5cc9a4fe0ab2a
ppxasjsm <antonia.mey@ed.ac.uk>
2020-03-13 15:34:12 +0000

    adding ph5 data

---
+++ data/D/gaff2/pH5/II_parmchk2.sh
@@ -0,0 +1,1 @@
+$AMBERHOME/bin/parmchk2 -i ade_D_ala_prep.mol2 -o ade_D_ala.frcmod -f mol2 -s 2
---
+++ data/D/gaff2/pH5/I_antechamber.sh
@@ -0,0 +1,3 @@
+#babel -ixyz ade-D-ala.xyz -opd ade-D-ala.pdb
+
+$AMBERHOME/bin/antechamber -i ade_D_ala.pdb -fi pdb -o ade_D_ala_prep.mol2 -fo mol2 -c 2 -eq 2 -at gaff2 -nc -1
